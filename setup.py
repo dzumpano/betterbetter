c112be398c10078d082f567c0b75b10c7b91b183
dzumpano <58341257+dzumpano@users.noreply.github.com>
2024-09-26 18:55:05 -0700

    check

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ import os
 if sys.platform != "win32":
     venvdir = os.path.abspath(os.path.join(os.getcwd(), 'venv\\Scripts'))
     #print(venvdir)
-    subprocess.run(['ls'])
-    subprocess.run(["./activate"])
+    subprocess.run([".workspace/venv/Scripts/activate"])
 
 import discord
 from io import BytesIO
